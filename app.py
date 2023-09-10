a8c116ec30238906b709849c8198532b4fd44c19
SIDDHARTH0760 <mcjsid73@gmail.com>
2023-09-10 16:56:06 +0530

    new

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler# used to standardize the data
-from sklearn.model_selection import train_test_split # used to split data for train and test
-from sklearn import svm
-from sklearn.metrics import accuracy_score
+# from sklearn.model_selection import train_test_split # used to split data for train and test
+# from sklearn import svm
+# from sklearn.metrics import accuracy_score
 import pickle
 import streamlit as st
 
@@ -13,7 +13,7 @@ st.title('Diabetes Prediction using ML')
     
     
     # getting the input data from the user
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
     
 with col1:
     Pregnancies = st.text_input('Number of Pregnancies')
@@ -21,37 +21,37 @@ with col1:
 with col2:
     Glucose = st.text_input('Glucose Level')
     
-with col3:
+with col1:
     BloodPressure = st.text_input('Blood Pressure value')
     
-with col1:
-    SkinThickness = st.text_input('Skin Thickness value')
-    
 with col2:
+    SkinThickness = st.text_input('Skin Thickness value(avg ~30)')
+    
+with col1:
     Insulin = st.text_input('Insulin Level')
     
-with col3:
+with col2:
     BMI = st.text_input('BMI value')
     
 with col1:
-    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function (avg ~0.47)')
     
 with col2:
     Age = st.text_input('Age of the Person')
     
     
-    # code for Prediction
-    diab_diagnosis = ''
+# code for Prediction
+diab_diagnosis = ''
     
-    # creating a button for Prediction
+# creating a button for Prediction
     
-    if st.button('Diabetes Test Result'):
-        diab_prediction = loaded_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+if st.button('Diabetes Test Result'):
+    diab_prediction = loaded_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
         
-        if (diab_prediction[0] == 1):
-          diab_diagnosis = 'The person is diabetic'
-        else:
-          diab_diagnosis = 'The person is not diabetic'
+    if (diab_prediction[0] == 1):
+        diab_diagnosis = 'The person is diabetic'
+    else:
+      diab_diagnosis = 'The person is not diabetic'
         
-    st.success(diab_diagnosis)
+st.success(diab_diagnosis)
 
